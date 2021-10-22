c13a1d6e8f99182ba65b06a219b96d1c11d0f7ad
bhartesh <bharteshsingh11@gmail.com>
2021-10-22 20:58:09 +0530

    demo project

---
+++ src/app/main-application/main-application.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainApplicationRoutingModule } from './main-application-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MainApplicationRoutingModule
+  ]
+})
+export class MainApplicationModule { }
